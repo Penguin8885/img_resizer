9f1db93ae4b4a8ced26cfdd4a6686602a5898453
Penguin8885 <penguin.8885@gmail.com>
2017-04-02 02:10:12 +0900

    make resize function

--- resize.py
+++ resize.py
@@ -4,6 +4,29 @@ import sys
 import os
 import shutil
 
+def resize(img):
+    img_h, img_w = img.shape[:2]   #画像サイズ
+    img_ratio = img_w / img_h      #画像サイズ縦横比
+
+    white_img = np.zeros((base_h, base_w, 3), np.uint8) #白塗り画像のベース作成
+    white_img[:,:] = [255, 255, 255]                    #白塗り
+
+    ###画像リサイズ, 白塗りにオーバーレイ
+    if img_ratio > base_ratio:
+        h = int(base_w/img_ratio)           #横から縦を計算
+        w = base_w                          #横を合わせる
+        resize_img = cv2.resize(img, (w,h)) #リサイズ
+    else:
+        h = base_h                          #縦を合わせる
+        w = int(base_h*img_ratio)           #縦から横を計算
+        resize_img = cv2.resize(img, (w,h)) #リサイズ
+
+    white_img[int(base_h/2-h/2):int(base_h/2+h/2),int(base_w/2-w/2):int(base_w/2+w/2)] = resize_img #オーバーレイ
+    resize_img = white_img #上書き
+
+    return resize_img
+
+
 if __name__ == '__main__':
     args = sys.argv
     if len(args) < 3:
@@ -32,24 +55,7 @@ if __name__ == '__main__':
         os.remove(read_tmp)
 
         #リサイズ
-        img_h, img_w = img.shape[:2]   #画像サイズ
-        img_ratio = img_w / img_h      #画像サイズ縦横比
-
-        white_img = np.zeros((base_h, base_w, 3), np.uint8) #白塗り画像のベース作成
-        white_img[:,:] = [255, 255, 255]                    #白塗り
-
-        ###画像リサイズ, 白塗りにオーバーレイ
-        if img_ratio > base_ratio:
-            h = int(base_w/img_ratio)           #横から縦を計算
-            w = base_w                          #横を合わせる
-            resize_img = cv2.resize(img, (w,h)) #リサイズ
-        else:
-            h = base_h                          #縦を合わせる
-            w = int(base_h*img_ratio)           #縦から横を計算
-            resize_img = cv2.resize(img, (w,h)) #リサイズ
-
-        white_img[int(base_h/2-h/2):int(base_h/2+h/2),int(base_w/2-w/2):int(base_w/2+w/2)] = resize_img #オーバーレイ
-        resize_img = white_img #上書き
+        resize_img = resize(img)
 
         #名前を変更した画像を書き込み(日本語名の画像をOpenCVが書き込めないため)，リネーム
         write_tmp = "./result/tmp.jpg"
